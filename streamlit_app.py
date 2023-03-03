174b4239899cd5c92cb8a64b53464a205bac15bb
smugala69 <126843049+smugala69@users.noreply.github.com>
2023-03-03 09:47:14 -0500

    Update streamlit_app.py
    Added Emojies
    added build your own smoothie text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')            
-streamlit.text('Hard-Boiled Free-Range Egg')            
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')            
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')            
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
